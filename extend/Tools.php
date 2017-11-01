c924b844950f4b9a48aad8bbfc219016ac30dd8d
fengyuexingzi <fengyuexingzi@gmail.com>
2017-11-01 17:38:07 +0800

    modified:   composer.json 	new file:   extend/Tools.php 	new file:   key/private.key 	new file:   key/public.key

---
+++ extend/Tools.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wind
+ * Date: 2017/11/1
+ * Time: 17:29
+ */
+
+namespace king;
+class Tools
+{
+    function aes($data, $method = 'encrypt', $key = '', $iv = '')
+    {
+        $key = $key ?: 'cTFbUKEMWHzP4yeRWpaXZf98NLY902IkxJ4XOuQIq1c=';
+        $iv = $iv ?: 'Bqa1wCAKcFwFMsYF';
+        if (strcasecmp($method, 'encrypt') === 0) {
+            return base64_encode(openssl_encrypt($data, 'aes-128-cbc', $key, OPENSSL_RAW_DATA, $iv));
+        } elseif (strcasecmp($method, 'decrypt') === 0) {
+            return openssl_decrypt(base64_decode($data), 'aes-128-cbc', $key, OPENSSL_RAW_DATA, $iv);
+        } else
+            return false;
+    }
+
+    function uuid()
+    {
+        return bin2hex(openssl_random_pseudo_bytes(32));
+    }
+}
